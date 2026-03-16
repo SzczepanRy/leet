5661c976f6bc6980bed8fa8fe1fb24257ccb7a55
unknown <szep.rydzewski@gmail.com>
2026-03-16 19:25:54 +0100

    add

--- merge-sort/count-range-sum.py
+++ merge-sort/count-range-sum.py
@@ -35,15 +35,15 @@ class Solution(object):
         """
 
         n = len(nums)
-        sums = [0]*n
-        sums[0] =nums[0]
+        #addind zero by pojedyńcze liczby wyłu ok , bo 2+0 =2
+        sums = [0]
 
-        for i in range(1,n):
-            sums[i]= sums[i-1]+nums[i]
+        for i in range(n):
+            sums.append(sums[-1]+nums[i])
 
         def mergeSums( p,r) :
 
-            if p-r <=1:
+            if r-p <=1:
                 return 0
 
             mid = (p+r)//2
@@ -54,23 +54,44 @@ class Solution(object):
 
             for i in range(p,mid):
 
+                # szumkamu , największą wartość < od lower  i  infinum k
                 while j < r and sums[j]- sums[i] < lower:
                     j+=1
 
-                while k < r  and sums[j]- sums[i] <=higher:
+                while k < r  and sums[k]- sums[i] <=upper:
+                    k+=1
 
 
                 count += (k-j)
 
 
-            prefix_sums[p:r] = sorted(prefix_sums[p:r])
+
+            merged = []
+            left_idx, right_idx = p, mid
+
+            while left_idx < mid and right_idx < r:
+                if sums[left_idx] <= sums[right_idx]:
+                    merged.append(sums[left_idx])
+                    left_idx += 1
+                else:
+                    merged.append(sums[right_idx])
+                    right_idx += 1
+
+            # Dodanie reszty elementów, które zostały
+            merged.extend(sums[left_idx:mid])
+            merged.extend(sums[right_idx:r])
+
+            # Podmiana w oryginalnej tablicy
+            sums[p:r] = merged
+
             return count
 
 
 
 
 
-        print(mergeSums(0 n))
+        res = mergeSums(0, len(sums))
+        return res
 
 
 
