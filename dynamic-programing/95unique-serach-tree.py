4331547a1730d83ea927dadb0d8d792d6bbf22ce
SzczepanRy <szep.rydzewski@gmail.com>
2026-05-06 16:48:15 +0200

    add

---
+++ dynamic-programing/95unique-serach-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[Optional[TreeNode]]
+        """
+
+        if n == 0 :
+            return []
+
+
+        def build(start , end):
+            if start > end:
+                return [None]
+
+
+            all=[]
+
+            for i in range(start, end):
+
+                leftSubtrees = build(start, i-1)
+                rightSubtrees = build(i+1, end)
+
+                for left in leftSubtrees:
+                    for right in rightSubtrees:
+                        root = TreeNode(i) # Tworzymy korzeń
+                        root.left = left   # Podpinamy lewe poddrzewo
+                        root.right = right # Podpinamy prawe poddrzewo
+                        all.append(root) # Dodajemy gotowe drzewo do listy
+
+            return all
+
+        return build(1, n)
+
+
+        build(1 , n)
