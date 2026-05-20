15fc6dba72073b004aa877a5069f6936c78d6db2
unknown <szep.rydzewski@gmail.com>
2026-05-20 20:29:04 +0200

    add

--- dynamic-programing/410split-arr-largest-sum.py
+++ dynamic-programing/410split-arr-largest-sum.py
@@ -7,14 +7,14 @@ class Solution(object):
         """
 
         n = len(nums)
-        prefix = [0]*n
+        prefix = [0]*(n+1)
         prefix[0] = nums[0]
 
-        for i in range(1, n ):
-            prefix[i] = nums[i] + prefix[i-1]
+        for i in range( n ):
+            prefix[i+1] = nums[i] + prefix[i]
 
 
-        dp = [[0]*(k+1) for i in range(n+1)]
+        dp = [[float("inf")]*(k+1) for i in range(n+1)]
         dp[0][0]=0
 
         # dp i j nim-max suma dla i elementów  oraz j podziałów
@@ -22,6 +22,26 @@ class Solution(object):
         print(prefix)
 
         for j in range(1, k +1):
+
+            for i in range(1, n +1):
+                # p to miejsce ostatniego cięcia
+                for p in range(j - 1, i):
+                    current_sum = prefix[i] - prefix[p]
+                    # Szukamy minimum z maksimów
+                    res = max(dp[p][j-1], current_sum)
+                    dp[i][j] = min(dp[i][j], res)
+
+        for i in dp:
+            print(i)
+
+        return dp[n][k]
+
+
+
+
+
+
+
 
 
 
